599509c0a612d9a2413d7486f8bfc1f5a0592198
Fernando Rojas <fer.rojas95@gmail.com>
2026-07-14 22:11:54 -0600

    Cambios 1ra fase actualizacion paquetes

--- src/app/(user)/success/page.tsx
+++ src/app/(user)/success/page.tsx
@@ -18,7 +18,7 @@ const SuccessPage = async ({ searchParams }: Props) => {
   let phoneNumber: string | null = null;
   if (process.env.STRIPE_SECRET_KEY && session_id) {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: "2025-07-30.basil",
+      apiVersion: "2025-08-27.basil",
     });
     try {
       const session = await stripe.checkout.sessions.retrieve(session_id);
